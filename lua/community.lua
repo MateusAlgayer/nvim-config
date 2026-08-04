efba05e1e26b58ff2c45c8a98ba4a0976d32e717
Mateus Roberto Algayer <mateusr.algayer@gmail.com>
2026-08-03 21:21:10 -0300

    feat: configs para algumas linguagens + 'boas práticas'

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,14 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
   { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.sql" },
+
+  -- NOTE: Remover eventualmente
+  { import = "astrocommunity.workflow.precognition-nvim" },
+  { import = "astrocommunity.workflow.bad-practices-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
 }
---
+++ lua/plugins/keymaps.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {},
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+}
