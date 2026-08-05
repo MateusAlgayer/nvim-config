00a139cbfaad03e4200fb9147bd8d60562d246c4
Mateus Roberto Algayer <mateusr.algayer@gmail.com>
2026-08-04 21:22:28 -0300

    feat: scroll centralizado

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -5,7 +5,10 @@ return {
     opts = {
       mappings = {
         -- first key is the mode
-        n = {},
+        n = {
+          ["<C-u>"] = { "<C-u>zz", desc = "Scroll up centered" },
+          ["<C-d>"] = { "<C-d>zz", desc = "Scroll down centered" },
+        },
         t = {
           -- setting a mapping to false will disable it
           -- ["<esc>"] = false,
